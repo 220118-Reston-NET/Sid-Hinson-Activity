b8a7598f9c98ee5737057cecc247aa43cc283236
Sid Hinson <octobersith@outlook.com>
2022-01-19 13:01:00 -0500

    Additions to Code

--- HelloWorld/Car.cs
+++ HelloWorld/Car.cs
@@ -6,8 +6,27 @@ namespace CarFunction
     public class Car
     {
 
+        //Field
+        //It is used to store the information and define the current state of the object
+        //Private means that only the class itself we can use that field
+        private string _color = "Blue";
+        private string _owner;
+        private int _fuel;
+        private int _mpg;
 
+        //Property
+        //They are in PascalCase
+        public string Color
+        {
+            //getter - make private keyword readable
+            get { return "The color is " +_color; }
 
+            //setter - make one of your private fields writable
+            set {_color = value; }
+
+        }
+
+        
     }   
 
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CarFunction;
+
 Console.WriteLine("Hello, World!");
+
+//NameofClass NameofVariable = new NameofClass();
+Car car1 = new Car();
+Console.WriteLine(car1.Color);
+car1.Color = "Red";
+Console.WriteLine(car1.Color);

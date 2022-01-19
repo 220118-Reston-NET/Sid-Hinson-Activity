23a8dfb22f10fbb72f87d5777cf75f17bd592f4a
Sid Hinson <octobersith@outlook.com>
2022-01-19 12:31:05 -0500

    First Commit

---
+++ HelloWorld/Car.cs
@@ -0,0 +1,13 @@
+//Namespace is agreat way to organize your code
+namespace CarFunction
+{ 
+    //This is how you create a class while using the class keyword
+    //Public is there to make a class viisible for everyone else
+    public class Car
+    {
+
+
+
+    }   
+
+}

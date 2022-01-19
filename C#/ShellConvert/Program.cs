66ef15f8f39aaadf0afb7d72f1d8436acf12adec
Sid Hinson <octobersith@outlook.com>
2022-01-19 16:04:26 -0500

    Additions to Programs

---
+++ C#/ShellConvert/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Sid Hinson
+//Program converted from shell script from first day1
+
+Console.Clear();
+bool repeat = true;
+string tx ="Thanks for Using this program!";
+string gb ="Press Enter to End Program! Goodbye!";
+
+Console.WriteLine("******************************************************");
+Console.WriteLine("* Welcome to the Wine Pairing Optimizer Application  *");
+Console.WriteLine("*----------------------------------------------------*");
+Console.WriteLine("*  We will find what wine is best with your dinner!  *");
+Console.WriteLine("*    Enter 1 if you are having steak for dinner!     *");
+Console.WriteLine("*   Enter 2 if you are having chicken for dinner!    *");
+Console.WriteLine("*    Enter 3 if you are having pork for dinner!      *");
+Console.WriteLine("*   Enter 4 if you are having seafood for dinner!    *");
+Console.WriteLine("*    Enter 5 if you are vegetarian for dinner!       *");
+Console.WriteLine("*         Enter 6 to exit this program!              *");
+Console.WriteLine("******************************************************");
+
+Console.WriteLine(" Please Enter a Number to make your selection!");
+string userInput1 = Console.ReadLine();
+
+while (repeat)
+{
+    
+    if (userInput == "1")
+    {
+       
+
+
+    }
+
+      else if(userInput == "2")
+    {
+        
+
+
+    }
+
+     else if(userInput == "3")
+    {
+        
+
+
+    } 
+
+     else if(userInput == "4")
+    {
+        
+    }
+
+     else if(userInput == "3")
+    {
+        
+
+
+    }
+
+     else if(userInput == "4")
+    {
+        
+
+
+    }
+
+     else if(userInput == "5")
+    {
+        
+
+
+    }
+
+    else if(userInput == "6")
+    {
+        Console.WriteLine(" Thank you for using the Win Selection Program!");
+        Console.WriteLine(" Press Enter to end the Program!");
+        Console.ReadLine();
+        repeat = false;
+    }
+
+    else
+    {
+        Console.WriteLine(" Command Not Found!!!!! ");
+        Console.WriteLine(" Please try Again!! Press Enter to end the Program!");
+        Console.ReadLine();
+        repeat = false;
+    }
+
+}
+
+
+
+
+

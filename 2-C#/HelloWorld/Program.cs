99839108508c84a1905ffe75d03feede26a9030e
Sid Hinson <octobersith@outlook.com>
2022-01-20 21:30:17 -0500

    Added Serialization Tutorials

--- 2-C#/HelloWorld/Program.cs
+++ 2-C#/HelloWorld/Program.cs
@@ -2,6 +2,7 @@
 using CarFunction;
 using CollectionFunction;
 using ConversionFunction;
+using SerializationFunction;
 
 //This will print whatever string you put in the parenthesis into the terminal
 // Console.WriteLine("Hello, World!");
@@ -64,5 +65,22 @@ using ConversionFunction;
 // }
 
 //Initialized Collection class
-// Collection collect = new Collection();
-// collect.CollectionMain();
+Collection collect = new Collection();
+collect.CollectionMain();
+
+Conversion.ConversionMain();
+
+Console.WriteLine("===Static Demo===");
+Conversion._num = 10;
+Conversion conv1 = new Conversion();
+Conversion conv2 = new Conversion();
+
+Console.WriteLine(conv1.GiveMeNumber());
+Console.WriteLine(conv2.GiveMeNumber());
+
+Conversion._num = 200;
+
+Console.WriteLine(conv1.GiveMeNumber());
+Console.WriteLine(conv2.GiveMeNumber());
+
+Serialization.SerialMain();

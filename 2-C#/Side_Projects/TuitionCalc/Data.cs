9897396b750a7edb37efd6165e1e120507cd3045
Sid Hinson <octobersith@outlook.com>
2022-01-22 20:12:24 -0500

    Third Commit

--- 2-C#/Side_Projects/TuitionCalc/Data.cs
+++ 2-C#/Side_Projects/TuitionCalc/Data.cs
@@ -4,10 +4,11 @@ namespace DataFunctions
     public class UserProgam
     
     {
+        // Name Security
         private string FirstName;
         private string LastName;
 
-
+        // Start Program
           public void ProgStart()
         {
                 Console.WriteLine("*************************************************");
@@ -21,7 +22,7 @@ namespace DataFunctions
                 Console.Clear();
         }
 
-
+        //Grab Name as a Test to set Private Field
         public void GetName()
         {
                 Console.WriteLine(" Please Enter your FIRST name ");
@@ -31,6 +32,8 @@ namespace DataFunctions
                 FirstName = inputFirst;
                 LastName = inputLast;
         }
+
+        //Welcome
         public void Welcome()
         {
                 Console.WriteLine($" Hello there");
@@ -38,6 +41,7 @@ namespace DataFunctions
                 Console.WriteLine(" Press Enter to continue ");
         }
 
+        //Display Menu Choices
         public void DisplayChoices()
         {
         Console.WriteLine("******************************************");
@@ -50,75 +54,56 @@ namespace DataFunctions
         }
     }
 
-
-      public class MGA
+      //College Object Class
+      public class College
     
     {
-        public double MgaStHours { get; set; }
-        public double MgaTutionRate = 113.67;
-        double MgaActRate = 3.00;
-        double MgaAthRate = 45.00;
-        double MgaHealthRate = 20.00;
-        double MgaParkRate = 10.00 ;
-        double MgaInstRate = 125.00;
-        double MgaRecRate = 165.00 ;
-        double MgaTecRate = 46.00 ;
+        //Class Variables
+        public double StHours { get; set; }
+        public double TuitionRate { get; set; }
+        public double ActRate { get; set; }
+        public double AthRate { get; set; }
+        public double HealthRate { get; set; }
+        public double ParkRate { get; set; }
+        public double InstRate { get; set; }
+        public double RecRate { get; set; }
+        public double TecRate { get; set; }
 
-        double FeeTotal = 527.64;
-
+        //Display Fees
         public void printFees()
         {
-            Console.WriteLine($" ***********************************************");
-            Console.WriteLine($" MGA Institutional Fees: {MgaTutionRate}");
-            Console.WriteLine($" Tution Rate per Hour: {MgaActRate}");
-            Console.WriteLine($" Activity Fee: {MgaActRate}");
-            Console.WriteLine($" Athletic Fee: {MgaAthRate}");
-            Console.WriteLine($" Health Fee: {MgaHealthRate}");
-            Console.WriteLine($" Parking Fee: {MgaParkRate}");
-            Console.WriteLine($" Institutional Fee: {MgaInstRate}");
-            Console.WriteLine($" Recreational Fee: {MgaRecRate}");
-            Console.WriteLine($" Technology Fee: {MgaRecRate}");
-            Console.WriteLine($" **********************************************");
+            Console.WriteLine($" *****************************************************");
+            Console.WriteLine($" Here are the MGA Institutional Fees:");
+            Console.WriteLine($" Tution Rate per Hour: {TuitionRate}");
+            Console.WriteLine($" Activity Fee: {ActRate}");
+            Console.WriteLine($" Athletic Fee: {AthRate}");
+            Console.WriteLine($" Health Fee: {HealthRate}");
+            Console.WriteLine($" Parking Fee: {ParkRate}");
+            Console.WriteLine($" Institutional Fee: {InstRate}");
+            Console.WriteLine($" Recreational Fee: {RecRate}");
+            Console.WriteLine($" Technology Fee: {TecRate}");
+            Console.WriteLine($" *****************************************************");
         }
        
-
-
-    }
-
-      public class CGTC
-    
-    {
-       
-        public double CgtcStHours { get; set; }
-        double CgtcTutionRate = 100.00;
-        double CgtcActRate = 40.00;
-        double CgtcAthRate = 0.00;
-        double CgtcHealthRate = 6.00;
-        double CgtcParkRate = 0.00;
-        double CgtcInstRate = 55.00;
-        double CgtcRecRate = 0.00;
-        double CgtcTecRate = 105.00;
-
-        double FeeTotal = 306.00;
-
-        public void printFees()
+       //Display Tution
+        public double DisplayTution()
         {
-            Console.WriteLine($" ***********************************************");
-            Console.WriteLine($" CGTC Institutional Fees: {CgtcTutionRate}");
-            Console.WriteLine($" Tution Rate per Hour: {CgtcActRate}");
-            Console.WriteLine($" Activity Fee: {CgtcActRate}");
-            Console.WriteLine($" Athletic Fee: {CgtcAthRate}");
-            Console.WriteLine($" Health Fee: {CgtcHealthRate}");
-            Console.WriteLine($" Parking Fee: {CgtcParkRate}");
-            Console.WriteLine($" Institutional Fee: {CgtcInstRate}");
-            Console.WriteLine($" Recreational Fee: {CgtcRecRate}");
-            Console.WriteLine($" Technology Fee: {CgtcRecRate}");
-            Console.WriteLine($" **********************************************");
-
+            return (StHours * TuitionRate) + StHours + TuitionRate + ActRate + AthRate + HealthRate + ParkRate + InstRate + RecRate + TecRate;
         }
 
+        //Constructor
+        public College(double hours, double rate, double activity, double athelete, double health, double park, double inst, double rec, double tech)
+        {
+        StHours = hours;
+        TuitionRate = rate;
+        ActRate = activity; 
+        AthRate = athelete; 
+        HealthRate = health;
+        ParkRate = park;
+        InstRate = inst;
+        RecRate = rec;
+        TecRate = tech;
+        }
     }
-
-
     
 }
--- 2-C#/Side_Projects/TuitionCalc/Program.cs
+++ 2-C#/Side_Projects/TuitionCalc/Program.cs
@@ -24,20 +24,40 @@ string input1 = Console.ReadLine();
         case 1:
             Console.WriteLine("You have chosen Middle Georgia State College.");
             Console.WriteLine("Please Enter the Number of Credit Hours you are Taking.");
-            int choice1 = Convert.ToInt32(Console.ReadLine());
-            MGA mga1 = new MGA();
+            string choice1 = Console.ReadLine();
+            int MgaHours = Int32.Parse(choice1);
+            College MGA = new College(MgaHours, 113.67, 3.00, 45.00, 20.00, 10.00, 125.00, 165.00, 46.00);
+            MGA.StHours = MgaHours;
+            MGA.printFees();
+            Console.WriteLine("Your Final Overall Semester Tution is");
+            MGA.DisplayTution();
+            Console.WriteLine("Program Will now restart. Press Enter to Continue");
+            Console.ReadLine();
+            Console.Clear();
             break;
+   
 
 
         case 2:
             Console.WriteLine("You have chosen Central Georgia Technical College");
             Console.WriteLine("Please Enter the Number of Credit Hours you are Taking.");
-            int choice2 = Convert.ToInt32(Console.ReadLine());
-            CGTC cgtc1 = new CGTC();
+            string choice2 = Console.ReadLine();
+            int CgtcHours = Int32.Parse(choice2);
+            College CGTC = new College(CgtcHours, 100.00, 40.00, 0.00, 6.00, 0.00, 55.00, 0.00, 105.00);
+            CGTC.StHours = CgtcHours;
+            CGTC.printFees();
+            Console.WriteLine("Your Final Overall Semester Tution is");
+            CGTC.DisplayTution();
+            Console.WriteLine("Program Will now restart. Press Enter to Continue");
+            Console.ReadLine();
+            Console.Clear();
             break;
 
         case 3:
-            Console.WriteLine("Thank you for using Tuition Calculator. Bye for now.");
+            Console.WriteLine("Thank you for using Tuition Calculator. Bye for now. Press Enter to Exit");
+            Console.ReadLine();
+            Console.Clear();
+            repeat = false;
             break;
 
         default:
@@ -51,5 +71,4 @@ string input1 = Console.ReadLine();
     {
            Console.WriteLine("You MUST type either [1] [2] [3]");
     }
-
 }
